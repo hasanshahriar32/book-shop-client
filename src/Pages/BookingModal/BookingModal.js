752c9fd10bde61eb22ed92769c49f97ef1937863
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-11-30 20:15:28 +0600

    register radio option

--- src/Pages/BookingModal/BookingModal.js
+++ src/Pages/BookingModal/BookingModal.js
@@ -17,6 +17,15 @@ const BookingModal = ({books}) => {
     const [book, setBook] = useState({})
     const time = new Date();
     const [currentTime, setcurrentTime] = useState(time);
+    const handleInputBlur = event => {
+      const field = event.target.name;
+      const value = event.target.value;
+      let time = new Date();
+      setcurrentTime(time);
+      const newBook = {...book,currentTime};
+      newBook[field] = value ;
+      setBook(newBook)
+  }
     const handleAddUser = event =>{
       event.preventDefault();
       fetch('http://localhost:5000/booking', {
@@ -29,21 +38,13 @@ const BookingModal = ({books}) => {
       .then(res => res.json())
       .then(data => {
           if(data.acknowledged){
-              // alert('Booked');
+              alert('Booked');
               event.target.reset();
           }
       })
   }
 
-  const handleInputBlur = event => {
-      const field = event.target.name;
-      const value = event.target.value;
-      let time = new Date();
-      setcurrentTime(time);
-      const newBook = {...book,currentTime}
-      newBook[field] = value;
-      setBook(newBook)
-  }
+  
     return (
         <>
             <button className='book_btn' onClick={handleShow}>
@@ -63,7 +64,7 @@ const BookingModal = ({books}) => {
         <Form.Control type="text" name="userName" onBlur={handleInputBlur} defaultValue={user?.displayName} disabled  />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="email" name="userEmail" onBlur={handleInputBlur} defaultValue={user?.email} disabled  />
+        <Form.Control type="email" name="userEmail" onBlur={handleInputBlur} value={user?.email} disabled />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Control type="text" name="price" onBlur={handleInputBlur}  defaultValue={`Price: ${resale_price}`} disabled/>
@@ -75,7 +76,7 @@ const BookingModal = ({books}) => {
         <Form.Control type="text" name="address" onBlur={handleInputBlur} placeholder="Address" />
       </Form.Group>
         <div className='text-center'>
-        <button type='submit' className='text-center book_btn pe-5 ps-5 mx-aut0' >
+        <button type='submit' className='text-center book_btn pe-5 ps-5 mx-aut0' onClick={handleClose}>
             Submit
           </button>
         </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,20 +1,34 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState,  } from 'react';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import './Register.css'
 import { AuthContext } from '../../Context/UserContext';
+import Loader from '../Utilities/Loader/Loader';
 
 
 const Register = () => {
     let title = "Book Shop -Register";
     document.title = title;
+    const navigate = useNavigate();
     const [error, setError] = useState(null);
-    const {createUser} = useContext(AuthContext)
+    const {createUser, loading} = useContext(AuthContext);
+    const location = useLocation();
+    const[role, setRole] = useState('buyer')
 
+    const from = location.state?.from?.pathname || '/'
+    if(loading){
+        return <Loader></Loader>
+    }
+
+    const handleChange = (e) => {
+        const info = e.target;
+        const value = info.value;
+        setRole(value);
+    }
     const handleSubmit = event =>{
         event.preventDefault();
         const form = event.target;
-        const name = form.name.value
+        const userName = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
         if(password.length < 6){
@@ -26,14 +40,37 @@ const Register = () => {
             const user = result.user;
             console.log(user);
             form.reset();
+            navigate(from, {replace: true});
+            const currentUser = {
+                name: userName,
+                userEmail: email,
+                roleIndentify: role,
+
+            };
+            fetch('http://localhost:5000/user', {
+                 method: 'POST',
+                 headers: {
+                'content-type': 'application/json'
+            },
+                body: JSON.stringify(currentUser)
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch((err) => console.error(err));
+
         })
+        .catch(error => console.error(error));
+        
+        
+        
+
         
     }
     return (
         <div className=' mx-auto login-container'>
             <h3 className='text'>Please Register Now!</h3>
             <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
                     <Form.Control type="text" name="name" placeholder="Enter name" required/>
                 </Form.Group>
@@ -41,11 +78,26 @@ const Register = () => {
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" name="email" placeholder="Enter email" required/>
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name="password" placeholder="Password" required/>
                 </Form.Group>
+                <div>
+                    <ul style={{listStyle: "none", padding:'0'}}>
+                        <li >
+                            <input className='me-2 mb-2' onChange={handleChange} type="radio" id="f-option" name='selector' value='Buyer' checked={role === "Buyer"} />
+                            <label for="f-option">Buyer</label>
+                            <div class="check"></div>
+                        </li>
+                        <li>
+                            <input className='me-2' onChange={handleChange}  type="radio" id="s-option" name='selector' value='Seller' checked={role === "Seller"} />
+                            <label for="s-option">Seller</label>
+                            <div class="check">
+                                <div class="inside"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
                 <button className='login'>Register</button>
                 <p className='text-white mt-3'>Already, have an account <Link to='/login'> Login</Link></p>
                 <p className='text-error'>{error}</p>
